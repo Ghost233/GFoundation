804cd7a06eb3d9aebc5156f6b4389656a91bc1ea
Ghost233 <only.yesc@gmail.com>
2014-09-20 22:33:31 +0800

    update
    Signed-off-by: Ghost233 <only.yesc@gmail.com>

--- GFoundation/GFoundation/GAFNetworkingHelper/GAFNetworkingHelper.h
+++ GFoundation/GFoundation/GAFNetworkingHelper/GAFNetworkingHelper.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+#import <AFNetworking.h>
+
 @interface GAFNetworkingHelper : NSObject
 
 + (void)protobufRequestWithMethod:(NSString*)method
@@ -15,5 +17,11 @@
                        parameters:(NSData*)data
                           success:(void (^)(AFHTTPRequestOperation *operation, NSData* responseObject))success
                           failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
++ (void)jsonRequestWithMethod:(NSString*)method
+                    URLString:(NSString*)url
+                   parameters:(NSDictionary*)data
+                      success:(void (^)(AFHTTPRequestOperation *operation, NSDictionary* responseObject))success
+                      failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
 
 @end

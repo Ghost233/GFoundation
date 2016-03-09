133c4a4d9f54f912dcf4f06dcf777e85472f2c95
Ghost233 <only.yesc@gmail.com>
2016-03-09 15:32:07 +0800

    init

---
+++ GFoundation/GFoundation.h
@@ -0,0 +1,19 @@
+//
+//  GFoundation.h
+//  GFoundation
+//
+//  Created by Ghost on 16/3/9.
+//  Copyright © 2016年 Ghost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GFoundation.
+FOUNDATION_EXPORT double GFoundationVersionNumber;
+
+//! Project version string for GFoundation.
+FOUNDATION_EXPORT const unsigned char GFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GFoundation/PublicHeader.h>
+
+
